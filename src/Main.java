f4c3b93c3f9d9c11838d0bda93a37498674e1cce
jeremy.gau <caliban3010@gmail.com>
2020-03-29 22:22:08 +0200

    Ajout des fonctions recursives

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        int[] c = {0, 1, 2, 3, 5, 2, 1, 5, 3, 3, 1, 0};
         int[] c = {0, 1, 2, 3, 5, 2, 1, 5, 3, 3, 1, 0};
-        System.out.println(Arrays.toString(couts(new int[12], c)));
-        System.out.println(etapes(new ArrayList<>(), couts(new int[12], c)));
-
+        long a = System.currentTimeMillis();
+        System.out.println(Arrays.toString(couts(new int[c.length], c)));
+        System.out.println(etapes(new ArrayList<>(), couts(new int[c.length], c)));
+        System.out.println(System.currentTimeMillis()-a);
+        a = System.currentTimeMillis();
+        System.out.println(Arrays.toString(constructCostTable(c)));
+        System.out.println(etapesTable(new ArrayList<>(), c));
+        System.out.println(System.currentTimeMillis()-a);
     }
 
     static int[] couts(int[] T, int[] c) {
@@ -20,6 +26,22 @@ public class Main {
             T[i] = Math.min(T[i - 1], Math.min(T[i - 2], T[i - 3])) + c[i];
         }
         return T;
+    }
+
+    static int[] constructCostTable(int[] c) {
+        int[] table = new int[c.length];
+        for (int i = 0; i < c.length; i++) {
+            table[i] = T(i, c);
+        }
+        return table;
+    }
+
+    //recursive
+    static int T(int i, int[] c) {
+        if (i == 1) return T(i-1, c) + c[i];
+        if (i == 2) return Math.min(T(i-1, c), T(i-2, c)) + c[i];
+        if (i > 2) return Math.min(T(i-1, c), Math.min(T(i-2, c), T(i-3, c))) + c[i];
+        return c[0];
     }
 
     static List<Integer> etapes(List<Integer> e, int[] T) {
@@ -34,6 +56,30 @@ public class Main {
                 }
             } else {
                 if (T[i - 1] > T[i - 3]) {
+                    e.add(i - 3);
+                    i -= 3;
+                } else {
+                    e.add(i - 1);
+                    i--;
+                }
+            }
+        }
+        return e;
+    }
+
+    //recursive
+    static List<Integer> etapesTable(List<Integer> e, int [] c) {
+        for (int i = c.length; i > 0 ;) {
+            if (T(i - 1, c) > T(i - 2, c)) {
+                if (T(i - 2, c) > T(i - 3, c)) {
+                    e.add(i - 3);
+                    i -= 3;
+                } else {
+                    e.add(i - 2);
+                    i -= 2;
+                }
+            } else {
+                if (T(i - 1, c) > T(i - 3, c)) {
                     e.add(i - 3);
                     i -= 3;
                 } else {
